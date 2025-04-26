b462f6d6226bdb63f8f4bd2eae9253dbe4013881
GraceWebpro <102426261+GraceWebpro@users.noreply.github.com>
2025-04-26 21:33:06 +0100

    fifth commit

--- src/components/PopularActors.jsx
+++ src/components/PopularActors.jsx
@@ -8,12 +8,12 @@ import '../styles/Main.css';
 
 const PopularActorsCarousel = () => {
   const popularActors = [
-    { id: 1, name: 'Tom Cruise', imageUrl: 'https://link-to-tomcruise.jpg' },
-    { id: 2, name: 'Zendaya', imageUrl: 'https://link-to-zendaya.jpg' },
-    { id: 3, name: 'Chris Hemsworth', imageUrl: 'https://link-to-chris.jpg' },
-    { id: 4, name: 'Florence Pugh', imageUrl: 'https://link-to-florence.jpg' },
-    { id: 5, name: 'Leonardo DiCaprio', imageUrl: 'https://link-to-leonardo.jpg' },
-    { id: 6, name: 'Anya Taylor-Joy', imageUrl: 'https://link-to-anya.jpg' },
+    { id: 1, name: 'Lee Min Ho', imageUrl: 'https://www.90daykorean.com/wp-content/webp-express/webp-images/uploads/2021/01/479px-140120_minho_lee_c-min.jpg.webp' },
+    { id: 2, name: 'Kim Soo Hyun', imageUrl: 'https://www.90daykorean.com/wp-content/webp-express/webp-images/uploads/2019/07/IqFjJrXCR_8MQ3uYAnWOH-YLRPHU-1024x683-min-768x512-min.jpg.webp' },
+    { id: 3, name: 'Soo Ji Sub', imageUrl: 'https://www.90daykorean.com/wp-content/webp-express/webp-images/uploads/2021/01/So_Ji-sub-min.jpg.webp' },
+    { id: 4, name: 'Lee Jong Suk', imageUrl: 'https://www.90daykorean.com/wp-content/webp-express/webp-images/uploads/2019/07/884eff572d154a8c808930bba89cc3be.jpeg.webp' },
+    { id: 5, name: 'Ji Chang Wook', imageUrl: 'https://www.90daykorean.com/wp-content/uploads/2019/07/Ji-Chang-Wook-768x512.jpeg' },
+    { id: 6, name: 'Kim Woo Bin', imageUrl: 'https://www.90daykorean.com/wp-content/uploads/2019/07/kim-woo-bin-min.jpg' },
     // Add more if you want
   ];
 
